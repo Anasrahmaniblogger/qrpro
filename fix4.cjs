50094e388872d65f4fd344e1b91d60639f6132f8
Rahmani International <rahmanianas24@gmail.com>
2026-07-18 18:16:26 +0530

    Delete fix4.cjs

--- fix4.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('user.html', 'utf8');
-content = content.replace(/    <script>window\.addEventListener\("DOMContentLoaded", initNotifications\);<\/body><\/html>/, '    </script>\n    <script>window.addEventListener("DOMContentLoaded", initNotifications);</script>\n</body>\n</html>');
-fs.writeFileSync('user.html', content);
