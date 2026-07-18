9b621dc926f5656fdb2cd72649312950f6d459ab
Rahmani International <rahmanianas24@gmail.com>
2026-07-18 18:16:39 +0530

    Delete fix_time_delay.cjs

--- fix_time_delay.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let userHtml = fs.readFileSync('user.html', 'utf8');
-
-userHtml = userHtml.replace(
-    'if (trigger !== triggerEvent) return;',
-    'if (trigger !== triggerEvent && !(triggerEvent === "on_load" && trigger === "time_delay")) return;'
-);
-
-fs.writeFileSync('user.html', userHtml);
